4d577e1ec49837077de42ef09339372ee6f29fdc
Bayer <bayernator@protonmail.com>
2026-01-04 22:46:01 +0100

    make trait methods async - add async-trait macro
    Signed-off-by: Bayer <bayernator@protonmail.com>

--- src/blockchain/source.rs
+++ src/blockchain/source.rs
@@ -1,11 +1,15 @@
 use crate::blockchain::Result;
 use crate::models::{OutPoint, Transaction};
+use async_trait::async_trait;
 
+#[async_trait]
 pub trait BlockchainDataSource {
-    fn get_transaction(txid: &str) -> Result<Transaction>;
-    fn get_spending_transaction(outpoint: &OutPoint) -> Result<Option<Transaction>>;
-    fn get_address_transactions(address: &str) -> Result<Vec<Transaction>>;
-    fn get_transactions_batch(txids: &[&str]) -> Result<Vec<Option<Transaction>>>;
-    fn get_spending_transactions_batch(outpoints: &[&OutPoint])
-    -> Result<Vec<Option<Transaction>>>;
+    async fn get_transaction(&self, txid: &str) -> Result<Transaction>;
+    async fn get_spending_transaction(&self, outpoint: &OutPoint) -> Result<Option<Transaction>>;
+    async fn get_address_transactions(&self, address: &str) -> Result<Vec<Transaction>>;
+    async fn get_transactions_batch(&self, txids: &[&str]) -> Result<Vec<Option<Transaction>>>;
+    async fn get_spending_transactions_batch(
+        &self,
+        outpoints: &[&OutPoint],
+    ) -> Result<Vec<Option<Transaction>>>;
 }
