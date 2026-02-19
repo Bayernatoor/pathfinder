6271147284b4f14dff0915009ad46311b913a3e0
Bayer <bayernator@protonmail.com>
2026-02-19 16:02:41 +0100

    implement get_spending_transaction for EsploraClient + add a test
    Signed-off-by: Bayer <bayernator@protonmail.com>

--- src/blockchain/esplora.rs
+++ src/blockchain/esplora.rs
@@ -1,8 +1,7 @@
 use crate::blockchain::{BlockchainDataSource, BlockchainError, Result};
 use async_trait::async_trait;
-use bitcoin::{OutPoint, Transaction, Txid, consensus::encode::deserialize_hex};
+use bitcoin::{Block, OutPoint, Transaction, Txid};
 use serde::Deserialize;
-use serde_json::{Value, json};
 
 pub struct EsploraClient {
     base_url: String,
@@ -17,12 +16,13 @@ impl EsploraClient {
         }
     }
 
+    // helper to prevent rate limiting on mempool.space
     async fn throttle(&self) {
         tokio::time::sleep(std::time::Duration::from_millis(100)).await;
     }
 }
 
-// Response structure for /tx/:txid/outspend/:vout
+/// Response structure for /tx/:txid/outspend/:vout
 #[derive(Deserialize, Debug)]
 struct OutspendResponse {
     spent: bool,
@@ -39,7 +39,7 @@ impl BlockchainDataSource for EsploraClient {
 
         let hex = self
             .client
-            .get(url)
+            .get(&url)
             .send()
             .await
             .map_err(|e| BlockchainError::NetworkFailure(e.to_string()))?
@@ -54,24 +54,91 @@ impl BlockchainDataSource for EsploraClient {
         &self,
         outpoint: bitcoin::OutPoint,
     ) -> Result<Option<bitcoin::Transaction>> {
-        todo!()
+        // protext againt mempool.space rate limiting
+        self.throttle().await;
+
+        let url = format!(
+            "{}/tx/{}/outspend/{}",
+            self.base_url, outpoint.txid, outpoint.vout
+        );
+
+        let response = self
+            .client
+            .get(&url)
+            .send()
+            .await
+            .map_err(|e| BlockchainError::NetworkFailure(e.to_string()))?;
+
+        // Immediately check for 404, would mean transaction id does not exist
+        if response.status() == 404 {
+            return Err(BlockchainError::NotFound(format!(
+                "Transaction {} not found",
+                outpoint.txid
+            )));
+        }
+
+        // Deserialize the response into our OutspendResponse Struct
+        let outspend: OutspendResponse = response
+            .json()
+            .await
+            .map_err(|e| BlockchainError::DataInconsistency(e.to_string()))?;
+
+        if !outspend.spent {
+            return Ok(None);
+        }
+
+        match outspend.txid {
+            // Fetch the full transaction data using the txid we just found
+            Some(txid) => self.get_transaction(txid).await.map(Some),
+            None => Err(BlockchainError::DataInconsistency(
+                "Outspend marked spent but no txid returned".to_string(),
+            )),
+        }
     }
+
     async fn get_address_transactions(
         &self,
-        address: bitcoin::Address,
+        _address: bitcoin::Address,
     ) -> Result<Vec<bitcoin::Transaction>> {
         todo!()
     }
     async fn get_transactions_batch(
         &self,
-        txids: &[bitcoin::Txid],
+        _txids: &[bitcoin::Txid],
     ) -> Result<Vec<Option<bitcoin::Transaction>>> {
         todo!()
     }
     async fn get_spending_transactions_batch(
         &self,
-        outpoints: &[bitcoin::OutPoint],
+        _outpoints: &[bitcoin::OutPoint],
     ) -> Result<Vec<Option<bitcoin::Transaction>>> {
         todo!()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[tokio::test]
+    async fn test_esplora_outspend() {
+        let client = EsploraClient::new("https://mempool.space/api".to_string());
+
+        let outpoint = bitcoin::OutPoint::new(
+            "a260cc34b85217c01f0f8a14d0213c9536952592f3022a0f414e7485a4b016ec"
+                .parse()
+                .unwrap(),
+            27,
+        );
+
+        let result = client.get_spending_transaction(outpoint).await;
+        assert!(result.is_ok());
+
+        let tx = result.unwrap();
+        assert!(tx.is_some());
+        assert_eq!(
+            tx.unwrap().compute_txid().to_string(),
+            "59c738932e6d039e76c7eb9da8d6e36def35ee9ed092fb678f6e469d1a160312"
+        );
     }
 }
